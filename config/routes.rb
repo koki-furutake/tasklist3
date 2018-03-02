ca1aa5aeb1f9306bd33543281cee35782c4ebf4c
koki.furutake <koki.furutake5@gmail.co.jp>
2018-03-02 07:55:23 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,24 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
+  #トップページのrouter
+  root to:"toppages#index"
   get 'toppages/index'
 
   root to: 'tasks#index'
-
   resources :tasks
+  
+  #サインアップのrouter
+  get 'signup', to: 'users#new'
+  resources :users
+  
+  #ログインのrouter
+  get 'login', to: 'sessions#new'            
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
 end
