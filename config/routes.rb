4b6a12f4146d0d6cec70f73e2c375fa7994abab7
koki.furutake <koki.furutake5@gmail.co.jp>
2018-03-03 04:41:04 +0000

    login commit

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,7 @@ Rails.application.routes.draw do
   #ログインのrouter
   get 'login', to: 'sessions#new'            
   post 'login', to: 'sessions#create'
+  
   delete 'logout', to: 'sessions#destroy'
   
 end
